6199faf43671c11feb5a1b213becd291a20ddc8f
clowning <1073128985@qq.com>
2021-03-25 23:26:56 +0800

    改变登录和为登录的页面状态

--- src/main/java/com/shuanghan/community/controller/AuthorizeController.java
+++ src/main/java/com/shuanghan/community/controller/AuthorizeController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.ServletRequestBindingException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class AuthorizeController {
 
@@ -18,14 +20,15 @@ public class AuthorizeController {
     private GithubProvider githubProvider;
     @Value("${github.client.id}")
     private String clienId;
-    @Value("0b5994ce0f86d00c05a6df04af8c2098b0f632a9")
+    @Value("${github.client.secret}")
     private String clientSecret;
     @Value("${github.redirect.uri}")
     private String redirectUri;
 
     @GetMapping("/callback")
-    public String callback (@RequestParam(name = "code") String code
-                          , @RequestParam(name = "state") String state
+    public String callback (@RequestParam(name = "code") String code,
+                            @RequestParam(name = "state") String state,
+                            HttpServletRequest request
                             ){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clienId);
@@ -36,6 +39,14 @@ public class AuthorizeController {
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
         System.out.println(user.getName());
-        return "index";
+        if(user!=null){
+            //登录成功,写cookie和session
+            request.getSession().setAttribute("user",user);
+            return "redirect:/";
+        }
+        else {
+            //登录失败重新登录
+            return "redirect:/";
+        }
     }
 }
